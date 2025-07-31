286b180352ed0b4f3c57ad55b13db48c56ff0141
Srishty <srish.saj@gmail.com>
2025-07-31 17:16:54 +0100

    Modifying main

--- src/fastcs_uArm/__main__.py
+++ src/fastcs_uArm/__main__.py
@@ -2,6 +2,15 @@
 
 from argparse import ArgumentParser
 from collections.abc import Sequence
+from fastcs_uArm.RobotIOC import RobotController
+from fastcs.transport.epics.ca.options import EpicsCAOptions, EpicsGUIOptions
+from fastcs.transport.epics.options import EpicsIOCOptions
+from pathlib import Path
+from fastcs import FastCS
+from fastcs.transport.epics.options import (
+    EpicsGUIOptions,
+    EpicsIOCOptions,
+)
 
 from . import __version__
 
@@ -19,6 +28,21 @@ def main(args: Sequence[str] | None = None) -> None:
     )
     parser.parse_args(args)
 
+    gui_options = EpicsGUIOptions(
+    output_path=Path(".") / "robot.bob", title="My Robot Controller"
+    )
+    epics_options = EpicsCAOptions(
+        gui=gui_options,
+        ca_ioc=EpicsIOCOptions(pv_prefix="ROBOT"),
+    )
+    # connection_settings = IPConnectionSettings("localhost", 25565)
+    fastcs = FastCS(RobotController(), [epics_options])
+
+    fastcs.create_gui()
+
+    fastcs.run()
+
 
 if __name__ == "__main__":
+
     main()
